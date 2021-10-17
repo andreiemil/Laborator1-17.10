165bf55fad3daa648ae15cba06ca09a89afc4501
andre <andre@DESKTOP-76L0DG6>
2021-10-17 14:54:40 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laborator1_17._10
+{
+    class Programe
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
